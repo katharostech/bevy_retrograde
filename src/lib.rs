129ad0acdb19bfecef21e8f687b64219577bb883
Zicklag <zicklag@katharostech.com>
2021-03-25 14:59:55 -0500

    Fix Doc Typo

--- src/lib.rs
+++ src/lib.rs
@@ -31,7 +31,6 @@
 //! - A super-simple hierarchy system
 //! - Scaled pixel-perfect rendering with three camera modes: fixed width, fixed height, and
 //!   letter-boxed
-//! - An [LDtk](https://ldtk.io) map loading plugin
 //! - An [LDtk](https://ldtk.io) map loading [plugin](./plugins/bevy_retro_ldtk)
 //!
 //! And features that we will work on adding later:
