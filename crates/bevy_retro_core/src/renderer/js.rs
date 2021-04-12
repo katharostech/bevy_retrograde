6b10c6b9197534b92484c3500635c6e8addf4392
Zicklag <zicklag@katharostech.com>
2021-04-12 12:32:50 -0500

    Remove JS Snippets and Use no-modules Target
    This is in preparation for a web worker task pool for long-running
    compute jobs.
    
    - Use wasm-bindgen `--target no-modules` mode
    - Convert JS snippet to pure Rust handler
    - Update justfile accordingly

--- crates/bevy_retro_core/src/renderer/js.rs
+++
@@ -1,7 +0,0 @@
-use super::BrowserResizeHandle;
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen(module = "/js/renderer_utils.js")]
-extern "C" {
-    pub fn setup_canvas_resize_callback(canvas: BrowserResizeHandle);
-}
