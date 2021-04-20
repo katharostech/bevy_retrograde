87865e207432cda1404225a27075807bb8da9dc8
Zicklag <zicklag@katharostech.com>
2021-04-20 12:11:03 -0500

    Remove the "2" From WebGL2
    Helps make the type names less misleading since we switched to webgl1.

--- crates/bevy_retro_core/src/renderer/luminance_web_sys.rs
+++ crates/bevy_retro_core/src/renderer/luminance_web_sys.rs
@@ -1,7 +1,5 @@
-//! This module is forked from the luminance_web_sys crate which we modify here until our PR is
-//! merged.
-//!
-//! https://github.com/phaazon/luminance-rs/pull/489
+//! This module is forked from the luminance_web_sys crate which we modify here
+//! to use WebGL1 instead of WebGL2.
 //!
 //! # License
 //! Copyright (c) 2020, Dimitri Sabadie <dimitri.sabadie@gmail.com>
@@ -47,82 +45,81 @@ use web_sys::{Document, HtmlCanvasElement, Window};
 /// web-sys errors that might occur while initializing and using the platform.
 #[non_exhaustive]
 #[derive(Debug)]
-pub enum WebSysWebGL2SurfaceError {
+pub enum WebSysWebGLSurfaceError {
     CannotGrabWindow,
     CannotGrabDocument,
-    CannotGrabWebGL2Context,
-    NoAvailableWebGL2Context,
+    CannotGrabWebGLContext,
+    NoAvailableWebGLContext,
     StateQueryError(StateQueryError),
 }
 
-impl WebSysWebGL2SurfaceError {
+impl WebSysWebGLSurfaceError {
     fn cannot_grab_window() -> Self {
-        WebSysWebGL2SurfaceError::CannotGrabWindow
+        WebSysWebGLSurfaceError::CannotGrabWindow
     }
 
     fn cannot_grab_document() -> Self {
-        WebSysWebGL2SurfaceError::CannotGrabDocument
+        WebSysWebGLSurfaceError::CannotGrabDocument
     }
 
-    fn cannot_grab_webgl2_context() -> Self {
-        WebSysWebGL2SurfaceError::CannotGrabWebGL2Context
+    fn cannot_grab_webgl_context() -> Self {
+        WebSysWebGLSurfaceError::CannotGrabWebGLContext
     }
 
-    fn no_available_webgl2_context() -> Self {
-        WebSysWebGL2SurfaceError::NoAvailableWebGL2Context
+    fn no_available_webgl_context() -> Self {
+        WebSysWebGLSurfaceError::NoAvailableWebGLContext
     }
 }
 
-impl fmt::Display for WebSysWebGL2SurfaceError {
+impl fmt::Display for WebSysWebGLSurfaceError {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         match *self {
-            WebSysWebGL2SurfaceError::CannotGrabWindow => {
+            WebSysWebGLSurfaceError::CannotGrabWindow => {
                 f.write_str("cannot grab the window node")
             }
-            WebSysWebGL2SurfaceError::CannotGrabDocument => {
+            WebSysWebGLSurfaceError::CannotGrabDocument => {
                 f.write_str("cannot grab the document node")
             }
-            WebSysWebGL2SurfaceError::CannotGrabWebGL2Context => {
+            WebSysWebGLSurfaceError::CannotGrabWebGLContext => {
                 f.write_str("cannot grab WebGL2 context")
             }
-            WebSysWebGL2SurfaceError::NoAvailableWebGL2Context => {
+            WebSysWebGLSurfaceError::NoAvailableWebGLContext => {
                 f.write_str("no available WebGL2 context")
             }
-            WebSysWebGL2SurfaceError::StateQueryError(ref e) => {
+            WebSysWebGLSurfaceError::StateQueryError(ref e) => {
                 write!(f, "WebGL2 state query error: {}", e)
             }
         }
     }
 }
 
-impl std::error::Error for WebSysWebGL2SurfaceError {}
+impl std::error::Error for WebSysWebGLSurfaceError {}
 
-impl From<StateQueryError> for WebSysWebGL2SurfaceError {
+impl From<StateQueryError> for WebSysWebGLSurfaceError {
     fn from(e: StateQueryError) -> Self {
-        WebSysWebGL2SurfaceError::StateQueryError(e)
+        WebSysWebGLSurfaceError::StateQueryError(e)
     }
 }
 
-/// web-sys surface for WebGL2.
-pub struct WebSysWebGL2Surface {
+pub struct WebSysWebGLSurface {
     pub window: Window,
     pub document: Document,
     pub canvas: HtmlCanvasElement,
     backend: Glow,
 }
 
-impl WebSysWebGL2Surface {
-    pub fn from_canvas(canvas: HtmlCanvasElement) -> Result<Self, WebSysWebGL2SurfaceError> {
+impl WebSysWebGLSurface {
+    pub fn from_canvas(canvas: HtmlCanvasElement) -> Result<Self, WebSysWebGLSurfaceError> {
         let window =
-            web_sys::window().ok_or_else(|| WebSysWebGL2SurfaceError::cannot_grab_window())?;
+            web_sys::window().ok_or_else(|| WebSysWebGLSurfaceError::cannot_grab_window())?;
         let document = window
             .document()
-            .ok_or_else(|| WebSysWebGL2SurfaceError::cannot_grab_document())?;
+            .ok_or_else(|| WebSysWebGLSurfaceError::cannot_grab_document())?;
 
         let webgl = canvas
             .get_context("webgl")
-            .map_err(|_| WebSysWebGL2SurfaceError::cannot_grab_webgl2_context())?
-            .ok_or_else(|| WebSysWebGL2SurfaceError::no_available_webgl2_context())?;
+            .map_err(|_| WebSysWebGLSurfaceError::cannot_grab_webgl_context())?
+            .ok_or_else(|| WebSysWebGLSurfaceError::no_available_webgl_context())?;
 
         let ctx = Context::from_webgl1_context(webgl.dyn_into().unwrap());
 
@@ -144,7 +141,7 @@ impl WebSysWebGL2Surface {
     }
 }
 
-unsafe impl GraphicsContext for WebSysWebGL2Surface {
+unsafe impl GraphicsContext for WebSysWebGLSurface {
     type Backend = Glow;
 
     fn backend(&mut self) -> &mut Self::Backend {
